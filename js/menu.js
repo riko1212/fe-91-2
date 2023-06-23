bd43bd5f873eb0cfc3179e1bc4ef9cfcd6273515
Andrii Danylko <46224717+riko1212@users.noreply.github.com>
2023-06-23 21:41:28 +0300

    +++

--- js/menu.js
+++ js/menu.js
@@ -12,3 +12,22 @@
     refs.menu.classList.toggle('is-hidden');
   }
 })();
+
+// (() => {
+//   const refs = {
+//     openMenuBtn: document.querySelector('[data-menu-open]'),
+//     closeMenuBtn: document.querySelector('[data-menu-close]'),
+//     menu: document.querySelector('[data-menu]'),
+//     burger: document.querySelector('.open-menu-btn span'),
+//   };
+
+//   refs.openMenuBtn.addEventListener('click', toggleMenu);
+//   refs.openMenuBtn.addEventListener('click', () => {
+//     refs.burger.classList.toggle('show');
+//   });
+//   refs.closeMenuBtn.addEventListener('click', toggleMenu);
+
+//   function toggleMenu() {
+//     refs.menu.classList.toggle('is-hidden');
+//   }
+// })();
